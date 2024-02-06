523c430433931cec8812b1d6d2aecd68fa57df44
Ruth Iruoaboroghene Felix <ruthflex@yahoo.co.uk>
2024-02-06 18:02:26 +0100

    created the plant show page

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,2 +1,11 @@
 class PlantsController < ApplicationController
+  def show
+    @plant = Plant.find(params[:id])
+  end
+
+  def destroy
+    @plant = Plant.find(params[:id])
+    @plant.destroy
+    redirect_to user_path(current_user)
+  end
 end
