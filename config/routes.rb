108b9cd0656ebfca444d6c4cbd8a5729ff58076e
Ruth Iruoaboroghene Felix <ruthflex@yahoo.co.uk>
2024-02-07 10:43:30 +0100

    accepted incoming changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-<<<<<<< HEAD
-  resources :plants, only: [:show, :edit, :update]
-=======
-  resources :plants, only: [:index]
->>>>>>> master
+  resources :plants, only: %i[show edit update] do
+    resources :plants, only: [:index]
+  end
 end
