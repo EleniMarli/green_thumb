388d618db1354ff8bc00d7a59ba6ef2b30d858ed
Ruth Iruoaboroghene Felix <ruthflex@yahoo.co.uk>
2024-02-07 17:46:29 +0100

    reminder pages created

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,2 +1,21 @@
 class RemindersController < ApplicationController
+  def new
+    @reminder = Reminder.new
+  end
+
+  def create
+    @reminder = Reminder.new(reminder_params)
+    @reminder.user = current_user
+    if @reminder.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reminder_params
+    params.require(:reminder).permit(:alarm_time)
+  end
 end
