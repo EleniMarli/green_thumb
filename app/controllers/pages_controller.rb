cceaa71c087f296f54beadfacec9eb4d72efc122
Eleni Marli <elenimarli@yahoo.gr>
2024-02-07 14:03:51 +0100

    Fix navbar to bottom

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @plants = Plant.all
+    @plants = Plant.where(user: current_user)
     @user = current_user
   end
 end
