b1b3a36f252d59ed504471b2dbad35fb70fd78c8
Eleni Marli <elenimarli@yahoo.gr>
2024-02-08 00:08:08 +0100

    Work on notifications system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,19 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  before_action :notify_user, only: [:home]
 
   def home
     @plants = Plant.where(user: current_user)
     @user = current_user
+  end
+
+  private
+
+  def notify_user
+    tasks = Task.where('next_date >= ? AND done = ?', Date.today, false)
+    unless tasks.empty?
+      flash[:notification] = "You have #{tasks.count} tasks"
+      tasks.update_all(done: true)
+    end
   end
 end
