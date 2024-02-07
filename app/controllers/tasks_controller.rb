a7ea35812bd6b2d30ffa3966c00dfe4b989c755b
Eva Ungerer <ungerer.eva@gmail.com>
2024-02-07 19:53:37 +0100

    improved view of task list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
     @tasks_today = Task.where("next_date = ?", Date.today)
-    @tasks_upcoming = Task.where("next_date > ?", Date.today).order(next_date: :asc)
+    @tasks_upcoming = Task.where(next_date: (Date.today + 1)..(Date.today + 7)).order(next_date: :asc)
   end
 end
