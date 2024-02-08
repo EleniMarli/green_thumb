af24a75369e07a4031f0e02fc9a2f7b905b229ad
Eleni Marli <elenimarli@yahoo.gr>
2024-02-08 14:08:25 +0100

    Add shown attribute to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_130403) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_130723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_130403) do
     t.bigint "plant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "shown", default: false
     t.index ["plant_id"], name: "index_tasks_on_plant_id"
   end
 
