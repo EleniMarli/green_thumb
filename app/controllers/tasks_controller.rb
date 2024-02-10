7d52150818c97639103aa69757937db2a8698bf4
Eleni Marli <elenimarli@yahoo.gr>
2024-02-10 01:09:00 +0100

    Add icons to empty views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
   def index
-    @incomplete_tasks_today = Task.where("date = ? AND done = ?", Date.today, false)
-                                  .order(delayed: :desc, date: :asc)
+    user_tasks = current_user.plants.includes(:tasks).flat_map(&:tasks)
+    unless user_tasks.empty?
+      @incomplete_tasks_today= @incomplete_tasks_today.where("date = ? AND done = ?", Date.today, false)
+                                                      .order(delayed: :desc, date: :asc)
+    end
     # @tasks_upcoming = Task.where(date: (Date.today + 1)..(Date.today + 7)).order(date: :asc)
   end
 
